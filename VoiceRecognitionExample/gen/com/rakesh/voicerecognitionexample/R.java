8b2c5a1c174bd32c1d72be6174619978f5f9d9d8
Compnerd21 <bsrsmart@gmail.com>
2014-12-17 13:41:18 -0500

    class

--- VoiceRecognitionExample/gen/com/rakesh/voicerecognitionexample/R.java
+++ VoiceRecognitionExample/gen/com/rakesh/voicerecognitionexample/R.java
@@ -37,7 +37,7 @@ public final class R {
         public static final int stat=0x7f090002;
         public static final int tShuff=0x7f09000a;
         public static final int textView1=0x7f090001;
-        public static final int textView2=0x7f090004;
+        public static final int textview=0x7f090004;
     }
     public static final class layout {
         public static final int activity_voice_recognition=0x7f030000;
